d91733499ce9ad066b01ef3cfd76b97bcfcb2e37
Andrii Soldatenko <andrii.soldatenko@gmail.com>
2023-12-30 22:25:27 +0100

    init basic transaction logger

---
+++ main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"sync"
+
+	"github.com/gorilla/mux"
+)
+
+var logger TransactionLogger
+
+var store = struct {
+	sync.RWMutex
+	m map[string]string
+}{m: make(map[string]string)}
+
+var ErrorNoSuchKey = errors.New("no such key")
+
+func Put(key, value string) error {
+	store.Lock()
+	store.m[key] = value
+	store.Unlock()
+	return nil
+}
+
+func Get(key string) (string, error) {
+	store.RLock()
+	value, ok := store.m[key]
+	store.RUnlock()
+
+	if !ok {
+		return "", ErrorNoSuchKey
+	}
+
+	return value, nil
+}
+
+func Delete(key string) error {
+	store.Lock()
+	delete(store.m, key)
+	store.Unlock()
+
+	return nil
+}
+
+func keyValuePutHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+
+	value, err := io.ReadAll(r.Body)
+	defer r.Body.Close()
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = Put(key, string(value))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+
+}
+
+func keyValueGetHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+
+	value, err := Get(key)
+	if errors.Is(err, ErrorNoSuchKey) {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write([]byte(value))
+}
+
+func initializeTransactionLog() error {
+	var err error
+	logger, err = NewFileTransactionLogger("transaction.log")
+	if err != nil {
+		return fmt.Errorf("failed to creat event logger: %w", err)
+	}
+
+	events, errors := logger.ReadEvents()
+	e, ok := Event{}, true
+
+	for ok && err == nil {
+		select {
+		case err, ok = <-errors:
+		case e, ok = <-events:
+			switch e.EventType {
+			case EventDelete:
+				err = Delete(e.Key)
+			case EventPut:
+				err = Put(e.Key, e.Value)
+			}
+		}
+	}
+
+	logger.Run()
+	return err
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/v1/{key}", keyValuePutHandler).Methods("PUT")
+	r.HandleFunc("/v1/{key}", keyValueGetHandler).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
