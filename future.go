2ac8e3cc43023856a826ac35cc83907786848875
Andrii Soldatenko <andrii.soldatenko@gmail.com>
2023-12-30 15:42:09 +0100

    init repo

---
+++ future.go
@@ -0,0 +1,36 @@
+package main
+
+import "sync"
+
+type Future interface {
+	Result() (string, error)
+}
+
+type InnerFuture struct {
+	once sync.Once
+	wg   sync.WaitGroup
+
+	res   string
+	err   error
+	resCh <-chan string
+	errCh <-chan error
+}
+
+func (f *InnerFuture) Result() (string, error) {
+	f.once.Do(
+		func() {
+			f.wg.Add(1)
+			defer f.wg.Done()
+
+			f.res = <-f.resCh
+			f.err = <-f.errCh
+
+		})
+	f.wg.Wait()
+
+	return f.res, f.err
+}
+
+func main() {
+
+}
